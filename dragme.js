1e45c83422aac84842f333916cdd9603df18ace9
Sam Manders <sammanders@outlook.com>
2018-06-11 14:23:54 +0200

    Added function

--- dragme.js
+++ dragme.js
@@ -1,44 +1,48 @@
 
 
+function DragMe (){
 
-
-function startDrag(e) {
-    if (!e) {
-        var e = window.event;
-    }
-    if(e.preventDefault) e.preventDefault();
     
-    targ = e.target ? e.target : e.srcElement;
+    
+    function startDrag(e) {
+        if (!e) {
+            var e = window.event;
+        }
+        if(e.preventDefault) e.preventDefault();
 
-    if (targ.className != 'dragme') {return};
-    offsetX = e.clientX;
-    offsetY = e.clientY;
+        targ = e.target ? e.target : e.srcElement;
 
-    if(!targ.style.left) { targ.style.left='0px'};
-    if (!targ.style.top) { targ.style.top='0px'};
+        if (targ.className != 'dragme') {return};
+        offsetX = e.clientX;
+        offsetY = e.clientY;
 
-    coordX = parseInt(targ.style.left);
-    coordY = parseInt(targ.style.top);
-    drag = true;
+        if(!targ.style.left) { targ.style.left='0px'};
+        if (!targ.style.top) { targ.style.top='0px'};
 
-    document.onmousemove=dragDiv;
-    return false;
+        coordX = parseInt(targ.style.left);
+        coordY = parseInt(targ.style.top);
+        drag = true;
+
+        document.onmousemove=dragDiv;
+        return false;
+
+    }
+    function dragDiv(e) {
+        if (!drag) {return};
+        if (!e) { var e= window.event};
+        targ.style.left=coordX+e.clientX-offsetX+'px';
+        targ.style.top=coordY+e.clientY-offsetY+'px';
+        return false;
+    }
+    function stopDrag() {
+        drag=false;
+    }
+    window.onload = function() {
+        document.onmousedown = startDrag;
+        document.onmouseup = stopDrag;
+    }
+
+
 
 }
-function dragDiv(e) {
-    if (!drag) {return};
-    if (!e) { var e= window.event};
-    targ.style.left=coordX+e.clientX-offsetX+'px';
-    targ.style.top=coordY+e.clientY-offsetY+'px';
-    return false;
-}
-function stopDrag() {
-    drag=false;
-}
-window.onload = function() {
-    document.onmousedown = startDrag;
-    document.onmouseup = stopDrag;
-}
-
-
 
